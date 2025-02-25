157aa1952adea676b492345f37ffc50ea1f07dab
mbc5674 <mbc5674@psu.edu>
2025-02-24 22:21:36 -0500

    primary, secondary, disabled buttons

--- design_library/src/app/app.component.spec.ts
+++ design_library/src/app/app.component.spec.ts
@@ -13,17 +13,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have the 'design_library' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('design_library');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, design_library');
-  });
 });
